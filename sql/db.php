7203820a0dc5665f45ff4b3e4a895da3fbcb4559
Cadilab <cadilab97@gmail.com>
2017-03-19 17:10:17 +0100

    connection
    simple connection

---
+++ sql/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$dsn = 'mysql:dbname=database;host=127.0.0.1';
+$user = 'user';
+$password = 'password';
+
+try 
+{
+    $dbh = new PDO($dsn, $user, $password);
+}
+catch (PDOException $e) 
+{
+    echo 'Connection failed: ' . $e->getMessage();
+}
